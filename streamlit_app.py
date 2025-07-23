3df6f530c42f4770347f45c511b387e7254e640b
ZhanbolEP <131301669+ZhanbolEP@users.noreply.github.com>
2025-07-23 15:49:48 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,5 @@
 import streamlit as st
-
-st.title('🎈 App Name')
-
-st.write('Hello world!')
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.tree import accuracy_score
+from sklearn.neighbors import 
